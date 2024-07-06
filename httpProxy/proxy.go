b0c6fb85d40208382584ddda358b10a229b8e8aa
hedykan <hedykan@outlook.com>
2024-07-06 18:08:24 +0800

    update proxy

--- httpProxy/proxy.go
+++ httpProxy/proxy.go
@@ -47,7 +47,7 @@ func GoReverseProxy(remote *url.URL, prefix string) *httputil.ReverseProxy {
 // 修改响应头构造函数
 func modifyResponseFunc() func(*http.Response) error {
 	return func(response *http.Response) error {
-		response.Header.Add("Access-Control-Allow-Origin", "*")
+		// response.Header.Add("Access-Control-Allow-Origin", "*")
 		return nil
 	}
 }
