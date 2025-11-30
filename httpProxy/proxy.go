c5362dfaa7b058c86c56ec0f10e33c725b3889fc
hedykan <hedykan@outlook.com>
2025-12-01 00:41:14 +0800

    Revert "update proxy"
    This reverts commit b0c6fb85d40208382584ddda358b10a229b8e8aa.

--- httpProxy/proxy.go
+++ httpProxy/proxy.go
@@ -71,7 +71,7 @@ func GoReverseProxy(remote *url.URL, prefix string, serviceName string) *httputi
 // 修改响应头构造函数
 func modifyResponseFunc() func(*http.Response) error {
 	return func(response *http.Response) error {
-		// response.Header.Add("Access-Control-Allow-Origin", "*")
+		response.Header.Add("Access-Control-Allow-Origin", "*")
 		return nil
 	}
 }
